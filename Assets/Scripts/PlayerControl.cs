f764f1f55cab9bb28bf4896750539ff0d49ffe6c
Emad Alsawahreh <74785005+EmadAlsawahreh@users.noreply.github.com>
2026-08-27 20:05:24 +0300

    Update scripts, scenes, and assets

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,35 +3,111 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerControl : MonoBehaviour
 {
-  
 
-    [SerializeField] 
+    //other scripts 
+
+    public SpawnManager spawnManager;
+    [SerializeField]
     private Vector2 mouse_Position;
+
+    // Perfect Growth Variables
+    [SerializeField] private float currentMass = 1f;
+    [SerializeField] private float massPerDot = 0.5f;
+    [SerializeField] private float visualMultiplier = 1f;
+
+
+    [SerializeField]
+    private float CameraGrowthAmount = 0.03f;
+
     private Rigidbody2D player_rigidbody;
 
-    [SerializeField] 
+    //public int x = spawnManager.max_x;
+    //public int y = spawnManager.max_y;
+    [SerializeField]
     private float move_Speed;
+
+    [SerializeField]
+    float cameraZ;
+
     void Start()
     {
         player_rigidbody = GetComponent<Rigidbody2D>();
 
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+
         // Mouse position
         Vector3 mouseScreen = Input.mousePosition;
         mouseScreen.z = 10f;
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreen);
         mouse_Position = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
-        print(mouse_Position);
-
 
         // moving the player to the mouse cursor 
-        player_rigidbody.transform.position = Vector3.MoveTowards(transform.position, mouse_Position, move_Speed * Time.deltaTime);
+        player_rigidbody.transform.position = Vector3.MoveTowards(transform.position, mouse_Position, move_Speed * Time.fixedDeltaTime);
+    }
+    // Update is called once per frame  
+    void Update()
+    {
 
     }
 
+
+    // other obecjt destoryer
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Dot"))
+        {
+            float playerScale = transform.localScale.x;
+            float dotScale = other.transform.localScale.x;
+
+            // check if the player is begger than dot +10%
+            if (playerScale >= dotScale * 1.10f)
+            {
+                float distance = Vector2.Distance(transform.position, other.transform.position);
+                float playerRadius = playerScale / 2f;
+                // check if the player reach the center of the dot 
+                if (distance <= playerRadius)
+                {
+                    // scale the player up
+                    currentMass += massPerDot;
+                    float calculatedScale = Mathf.Sqrt(currentMass) * visualMultiplier;
+                    transform.localScale = new Vector3(calculatedScale, calculatedScale, 1f);
+
+                    // zoom out the camera 
+                    Vector3 newCameraPos = Camera.main.transform.position;
+                    newCameraPos.z -= CameraGrowthAmount;
+                    Camera.main.transform.position = newCameraPos;
+
+                    // destroy dots and edit the  values
+                    SpawnManager.activeDotsCounter -= 1;
+                    Destroy(other.gameObject);
+                    SpawnManager.destroyedDotsCounter += 1;
+                }
+            }
+            // change the layer depend on size
+            else if (dotScale > playerScale)
+            {
+                SpriteRenderer dotRenderer = other.GetComponent<SpriteRenderer>();
+                if (dotRenderer != null)
+                {
+                    dotRenderer.sortingOrder = 3; 
+                }
+            }
+        }
+    }
+
+    // after player exit the dot return the layer to default
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Dot"))
+        {
+            SpriteRenderer dotRenderer = other.GetComponent<SpriteRenderer>();
+            if (dotRenderer != null)
+            {
+                dotRenderer.sortingOrder = 1; 
+            }
+        }
+    }
 
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -15,7 +16,7 @@ public class SpawnManager : MonoBehaviour
 
     [SerializeField]
     private GameObject dot_prefab;
-
+    
 
     [SerializeField]
     private float max_dot_scaling = 2f;
@@ -23,29 +24,74 @@ public class SpawnManager : MonoBehaviour
     // dot spawn area
     public float max_x = 50;
     public float max_y = 28;
+    [SerializeField] 
+    private int maxDotsAllowed = 100;
 
 
+    // active dots
+    static public int activeDotsCounter;
+    
+    // dots destroyed counter
+    static public int destroyedDotsCounter;
+
+
+    [SerializeField] 
+    private TextMeshProUGUI uiEatedDotCounter;
+
+
+    [SerializeField] 
+    private TextMeshProUGUI uiActiveDotsCounter;
+
+    [SerializeField]
+    private float dotsSpawnSpeed = 0.05f;
+
+
+    
+    [SerializeField] 
+    private float growthAmount = 0.05f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
+        activeDotsCounter = 0;
+        destroyedDotsCounter = 0;
+        InvokeRepeating(nameof(tryToSpawnADot), 0.5f, dotsSpawnSpeed);
+        
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-
-    }
     void Update()
     {
-        Vector2 randomPos = new Vector2(UnityEngine.Random.Range(-max_x, max_x), UnityEngine.Random.Range(-max_y, max_y));
-        Instantiate(dot_prefab, randomPos, Quaternion.identity);
-        SpriteRenderer colorizer = dot_prefab.GetComponent<SpriteRenderer>();
-        colorizer.color = randomizeDotsColor();
-        float randomScale = randomizeDotsScale(max_dot_scaling);
-        dot_prefab.transform.localScale = new Vector3(randomScale, randomScale, 1f);
+        
+        uiEatedDotCounter.text = destroyedDotsCounter.ToString();
+        uiActiveDotsCounter.text = activeDotsCounter.ToString();
+
+       
+
     }
+
+
+    // spawn dots method
+    void tryToSpawnADot()
+    {
+        if (activeDotsCounter >= maxDotsAllowed) return;
+
+            
+            
+            Vector2 randomPos = new Vector2(UnityEngine.Random.Range(-max_x, max_x), UnityEngine.Random.Range(-max_y, max_y));
+            GameObject newDot = Instantiate(dot_prefab, randomPos, Quaternion.identity);
+            SpriteRenderer colorizer = newDot.GetComponent<SpriteRenderer>();
+            colorizer.color = randomizeDotsColor();
+            float randomScale = randomizeDotsScale(max_dot_scaling);
+            newDot.transform.localScale = new Vector3(randomScale, randomScale, 1f);
+            activeDotsCounter += 1;
+    }
+
+    // caller of spawn dots method
+    
+
 
     // Scaling the dots
     private float randomizeDotsScale( float max_scaling) 
@@ -57,8 +103,15 @@ public class SpawnManager : MonoBehaviour
     // Coloring the dots
     private Color randomizeDotsColor() 
     {
-        int randomScale = UnityEngine.Random.Range(0, 5);
+        int randomScale = UnityEngine.Random.Range(0, dotsColorList.Length);
         return dotsColorList[randomScale];
     }
     
 }
+
+
+//editing the original prefab ==== 
+//Instantiate(dot_prefab, randomPos, Quaternion.identity);
+//SpriteRenderer colorizer = dot_prefab.GetComponent<SpriteRenderer>();
+
+//if (activeDotsCounter >= maxDotsAllowed) return; this idea way better than my if  > 5 
